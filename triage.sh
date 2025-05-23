4d3920bb65b789d504ad4d3369b61d80dbe670be
N0v4chr0n0 <willybolo2000@gmail.com>
2025-05-23 21:01:45 +0000

    MAJ

--- triage.sh
+++ triage.sh
@@ -5,6 +5,11 @@ sleep 1
 echo We are looking for your next mission.
 sleep 1
 echo Please wait...
-sleep 3
-echo 'NoFD_{HxH}' > /home/hunter/instruction.txt
-echo "Your instructions for the next mission is saved in /home/hunter/instruction.txt"
+if grep -q 'Accepted password for hunter from 172.66.0.3' /var/log/auth.log; then
+	sleep 3
+	echo 'NoFD_{HxH}' > /home/hunter/instruction.txt
+	echo "Your instructions for the next mission is saved in /home/hunter/instruction.txt"
+else
+	sleep 3
+	echo 'you have to connect to this machine via host_1 !! Good Luck :)'
+fi
